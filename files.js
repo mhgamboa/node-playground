252fa47e9cef36e652de09957511c9e5692b3848
Marcus <marcus.gamboa@gmail.com>
2021-08-24 11:48:18 -0600

    Create/Delete files

--- files.js
+++ files.js
@@ -2,5 +2,22 @@ const fs = require("fs");
 
 fs.readFile("./docs/blog1.txt", (err, data) => {
   if (err) console.log(err);
-  console.log(data.toString());
+  console.log(`Here is blog one: ${data.toString()}`);
 });
+
+console.log(fs.existsSync("./docs")); //checks if folder exists
+
+console.log(fs.existsSync("./docs/blog1.txt")); //checks if folder exists
+console.log(fs.existsSync("./docs/blog2.txt")); //checks if folder exists
+
+if (fs.existsSync("./assets")) {
+  fs.rmdir("./assets", (err) => {
+    if (err) console.log(err);
+  });
+  console.log("Asset folder deleted");
+} else {
+  fs.mkdir("./assets", (err) => {
+    if (err) console.log(err);
+  });
+  console.log("Asset folder created");
+}
