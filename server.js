c8fa2fbee9ffe1a1fd50d33dedb4cd723d5149ba
Marcus <marcus.gamboa@gmail.com>
2021-08-25 19:39:20 -0600

    Practice sending a response

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const portNumber = 3000;
 
 const server = http.createServer((req, res) => {
   console.log(`Server recived a request`);
+  console.log(req.url, req.method);
+
+  res.setHeader("Content-Type", "text/plain");
+  res.write("sup");
+  res.end();
 });
 
 server.listen(portNumber, "localhost", () => {
