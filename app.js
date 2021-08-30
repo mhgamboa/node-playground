c8ca48bd4b4c8933580fe74530127b821cae09a8
Marcus <marcus.gamboa@gmail.com>
2021-08-30 16:07:21 -0600

    Add an array of data into template view

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ app.set("view engine", "ejs");
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { title: "Home Page" });
-});
-
-app.get("/about", (req, res) => {
   const blogs = [
     { title: "You", snippet: "You are cool." },
     { title: "Me", snippet: "I am cool" },
     { title: "She", snippet: "She is cool" },
   ];
+  res.render("index.ejs", { title: "Home Page", blogs });
+});
+
+app.get("/about", (req, res) => {
   res.render("about", { title: "About Me" });
 });
 
